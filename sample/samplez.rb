82c558aea252f7c3976fd79bc32791951d04af47
siranovel <siranovel@gmail.com>
2023-06-20 13:35:00 +0900

    Add files via upload

--- sample/samplez.rb
+++ sample/samplez.rb
@@ -7,15 +7,28 @@ class Num4MechaEquTest
         @h0 = 0.0
         @v0 = 1.0
     end
-    def springFreqEquTest
+    def motiEqu4freeFallEquTest
+        puts 'motiEqu4freeFallTest in'
         yi_1 = []
-        yi_1 = Num4MechaEquLib.springFreqEqu(@k, @m, @h0, @v0)
+        yi_1 = Num4MechaEquLib.motiEqu4freeFallEqu(@m, @h0, @v0)
         yi_1.map{ |v|
             print v
             puts
         }
+        puts
+    end
+    def springFreqEquTest
+        puts 'springFreqEquTest in'
+        yi_1 = []
+        yi_1 = Num4MechaEquLib.springFreqEqu(@k, @m, 1, 0)
+        yi_1.map{ |v|
+            print v
+            puts
+        }
+        puts
     end
 end
- tst = Num4MechaEquTest.new
- tst.springFreqEquTest()
+tst = Num4MechaEquTest.new
+tst.springFreqEquTest()
+tst.motiEqu4freeFallEquTest()
 
